160147ea5fba128f1123ace8a01103c1235fbba7
Anton <odianboy@mail.ru>
2021-06-25 21:00:23 +0300

    initialization

---
+++ 04-board-sources/app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board')
+const colors = ['#55D3F7', '#2A65E3', '#9F3DF3', '#F33DD0', '#F33D3D', '#3DF38E', '#1DCA0E', '#DCF300', '#FFAE01', '#FF0101']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+        setColor(square))
+
+    square.addEventListener('mouseleave', () => 
+    removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+
+    return colors[index]
+}
